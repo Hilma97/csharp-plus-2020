2af0893daf802f0545f3971c381f55882debe303
hilma97 <hilmabergh@icloud.com>
2020-08-26 11:55:16 +0200

    testinbg2

---
+++ Session02/Session02exercise01/session02exercise2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace session02exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("do you want to enter your name? (y/n)?");
+            var key = Console.ReadKey();
+
+            if (key.KeyChar == 'n')
+                return;
+
+            Console.WriteLine("Enter your name:");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + name);
+            Console.ReadKey();
+
+        }
+    }
+}
